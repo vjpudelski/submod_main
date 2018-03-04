23bb9d9b8deb948d6ec60e949f3e4955781ff131
Victor J. Pudelski <vjpudelski@gmail.com>
2018-03-03 22:19:22 -0500

    added submodule

--- submod_main/Program.cs
+++ submod_main/Program.cs
@@ -7,6 +7,8 @@ namespace submod_main
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine(submod_class.Math.Add(1, 4));
         }
     }
 }
