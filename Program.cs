8b09fafa7be8b72091971b1c81cf50f36ce3c38c
Victor J. Pudelski <vjpudelski@gmail.com>
2018-03-03 21:37:51 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace submod_main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
